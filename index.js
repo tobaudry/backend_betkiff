d3fe3f563860cbec0589ff4989eaa124922f739b
Baudry <tobaudry@ensc.fr>
2025-01-14 23:51:03 +0100

    test

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+
+admin.initializeApp(); // Initialise Firebase Admin SDK
 
 const app = express();
 
-// Utilisation de CORS
+// Middleware pour gérer les CORS
 app.use(cors({ origin: true }));
 
-// Route de test
-app.get('/test', (req, res) => {
-  res.json({ message: 'Backend is working!' });
-});
+// Importer les routes
+const usersRoutes = require("./routes/users");
 
-// Démarrer le serveur
+// Utiliser les routes
+app.use("/users", usersRoutes);
+
+// Définir le port d'écoute
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const usersController = require("../controllers/usersController");
+
+// Routes utilisateurs
+router.post("/", usersController.getUsers);
+router.post("/ById/:uid", usersController.getUserById);
+router.post("/updateMonnaie", usersController.updateUserMoney);
+router.delete("/deleteUsers/:userId", usersController.deleteUser);
+router.put("/updateStatus", usersController.updateStatut);
+router.put("/updateUserLastOpening", usersController.updateUserLastOpening);
+router.get("/getIdOrgaByIdUser/:idUser", usersController.getIdOrgaByIdUser);
+
+module.exports = router;
