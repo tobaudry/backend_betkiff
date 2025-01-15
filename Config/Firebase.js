93bbf0eb543d58ce348accc1d09d5ecd2843e0fc
Fabien-Pvr <118728342+Fabien-Pvr@users.noreply.github.com>
2025-01-15 18:08:22 +0100

    variable

--- Config/Firebase.js
+++ Config/Firebase.js
@@ -4,10 +4,10 @@ const admin = require("firebase-admin");
 if (admin.apps.length === 0) {
   const serviceAccount = {
     type: "service_account",
-    project_id: "betkiff-5148f",
+    project_id: "enfc-pari",
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,  // ID de la clé privée
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Clé privée (replacer les \n échappés)
-    client_email: "firebase-adminsdk-vq9ev@betkiff-5148f.iam.gserviceaccount.com",
+    client_email: "firebase-adminsdk-td7zl@enfc-pari.iam.gserviceaccount.com",
     client_id: process.env.FIREBASE_CLIENT_ID,  // ID du client
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
