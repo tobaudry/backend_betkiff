70a573b08e4d88a833a0c7d16b901ea33e48397f
Tom BAUDRY <tbaudry@MacBook-Pro-de-admin4sh-2.local>
2025-03-03 11:38:56 +0100

    recup open pack

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -5,8 +5,66 @@ const db = admin.database();
 const PROBA_ULTRA_RARE = 0.05;
 const PROBA_RARE = 0.25;
 
+const PROBA_GOLD = 0.84;
+const PROBA_OBJECT = 0.89;
+const PROBA_PREZ = 0.9;
+
 const openPack = async (req, res) => {
-  const { idUser, cardsData, idOrganisation,idCollection } = req.body;
+  const { idUser, cardsData, idOrganisation } = req.body;
+  try {
+    const randomNumber = Math.random();
+    let drawnCard;
+    if (randomNumber < PROBA_GOLD) {
+      drawnCard =
+        cardsData.goldCard[
+          Math.floor(Math.random() * cardsData.goldCard.length)
+        ];
+    } else if (randomNumber < PROBA_OBJECT) {
+      drawnCard =
+        cardsData.objectCards[
+          Math.floor(Math.random() * cardsData.objectCards.length)
+        ];
+    } else if (randomNumber < PROBA_PREZ) {
+      drawnCard =
+        cardsData.prezCards[
+          Math.floor(Math.random() * cardsData.prezCards.length)
+        ];
+    } else {
+      drawnCard =
+        cardsData.possibleCards[
+          Math.floor(Math.random() * cardsData.possibleCards.length)
+        ];
+    }
+
+    // Références à la base de données
+    const userRef = db.ref(
+      `organisations/${idOrganisation}/users/${idUser}/collection`
+    );
+    const cardRef = db.ref(
+      `organisations/${idOrganisation}/users/${idUser}/collection/${drawnCard.title}`
+    );
+
+    // Utilisez `once('value')` au lieu de `get()`
+    const snapshot = await cardRef.once("value");
+
+    if (snapshot.exists()) {
+      const currentDoublon = snapshot.val().doublon || 1;
+      await cardRef.update({ doublon: currentDoublon + 1 });
+    } else {
+      await userRef.update({
+        [drawnCard.title]: { doublon: 1 },
+      });
+    }
+
+    res.status(200).json({ success: true, card: drawnCard });
+  } catch (error) {
+    console.error("Erreur lors de l'ouverture du pack :", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const openPackNew = async (req, res) => {
+  const { idUser, cardsData, idOrganisation, idCollection } = req.body;
   try {
     const randomNumber = Math.random(); // Génère un nombre entre 0 et 1
 
@@ -16,7 +74,7 @@ const openPack = async (req, res) => {
         cardsData.ultraRareCard[
           Math.floor(Math.random() * cardsData.ultraRareCard.length)
         ];
-    } else if (randomNumber < PROBA_RARE) {
+    } else if (randomNumber < PROBA_RARE && randomNumber >= PROBA_ULTRA_RARE) {
       drawnCard =
         cardsData.rareCard[
           Math.floor(Math.random() * cardsData.rareCard.length)
@@ -30,10 +88,10 @@ const openPack = async (req, res) => {
 
     // Références à la base de données
     const userRef = db.ref(
-      `organisations/${idOrganisation}/users/${idUser}/collection/${idCollection}`,
+      `organisations/${idOrganisation}/users/${idUser}/collection/${idCollection}`
     );
     const cardRef = db.ref(
-      `organisations/${idOrganisation}/users/${idUser}/collection/${idCollection}/${drawnCard.title}`,
+      `organisations/${idOrganisation}/users/${idUser}/collection/${idCollection}/${drawnCard.title}`
     );
 
     // Utilisez `once('value')` au lieu de `get()`
@@ -56,13 +114,13 @@ const openPack = async (req, res) => {
 };
 
 const getCards = async (req, res) => {
-  const { idOrganisation,idCollection } = req.body; // On récupère l'ID de la collection
+  const { idOrganisation, idCollection } = req.body; // On récupère l'ID de la collection
   const dbPath = `organisations/${idOrganisation}/collections/${idCollection}/urls`;
 
   db.ref(dbPath)
     .once("value")
     .then((snapshot) => {
-      const urls = snapshot.val(); 
+      const urls = snapshot.val();
 
       if (!urls || urls.length === 0) {
         return res.status(400).json({ message: "Aucune carte trouvée." });
@@ -74,5 +132,7 @@ const getCards = async (req, res) => {
 };
 
 module.exports = {
-  openPack,getCards,
+  openPack,
+  openPackNew,
+  getCards,
 };
